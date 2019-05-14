3f8ff5bceb027c4cd0bbd7570a8c97196402c125
Cognitifai <akash.nag@cognitif.ai>
2019-05-14 16:39:08 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -1,24 +1,17 @@
 const wsServer=require('ws').Server;
 const server=new wsServer({port:5001});
-let names=[];let c=0;
+
 server.on('connection',(ws)=>{
     console.log('Connection established..');    
-    ws.on('message',(message)=>{
-         
-        console.log(message);
-        let data=JSON.parse(message);
-        if(data.type=='name'){
-            names.push(data.data);
-        }
-        else if(data.type=='message'){               
+    ws.on('message',(message)=>{    
+        let data=JSON.parse(message);       
+        if(data.type=='message'){               
             server.clients.forEach((client)=>{                 
                 if(client!=ws){                                    
                     client.send(`${data.name}:${data.data}`); 
                 }                     
             })   
-        }
-        console.log(names);
-        
+        }      
     })
 
     ws.on('close',()=>{
